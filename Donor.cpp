e9e5b639062b5871e212a4e4c5845150713ef193
IT21161056 <99235396+AnojPeiris@users.noreply.github.com>
2022-05-09 06:11:41 +0000

    Edit

---
+++ Donor.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Donor{
+
+	protected:
+		char donorID[10];
+		char NIC[12];
+		char gender[6];
+		char dob[10];
+		char bloodGrp[4];
+		
+	public:
+		Donor(){
+      strcpy(donorID, "");
+      strcpy(NIC, "");
+      strcpy(gender, "");
+      strcpy(dob, "");
+      strcpy(bloodGrp, "");
+    }
+		Donor(const char dID[], const char nic[], const char gnder[], const char dateofBrth[], const char bgrp[]){
+      strcpy(donorID, dID);
+      strcpy(NIC, nic);
+      strcpy(gender, gnder);
+      strcpy(dob, dateofBrth);
+      strcpy(bloodGrp, bgrp);
+    }
+		void displayDonorDetails(){
+      
+    }
+};
